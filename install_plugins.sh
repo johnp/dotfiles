50095d4de00389a1b514f56122435641d6324352
Johannes Pfrang <johannespfrang@gmail.com>
2016-01-26 23:21:19 +0100

    Update installation routine and add README.

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,14 +1,8 @@
 #/bin/sh
 
-# goto home
-cd $HOME
-
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# install custom plugins
+# custom plugins
 git submodule add --depth 1 https://github.com/djui/alias-tips.git $HOME/.oh-my-zsh/custom/plugins/alias-tips
 git submodule add --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-# install custom themes
+# custom themes
 git submodule add --depth 1 https://github.com/bhilburn/powerlevel9k.git $HOME/.oh-my-zsh/custom/themes/powerlevel9k
